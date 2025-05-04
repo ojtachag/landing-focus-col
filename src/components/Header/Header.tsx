dd022f4b37fcb46db1b642c2580dd7b4f6b25deb
Oscar Tacha <oskr_tk@hotmail.com>
2025-05-03 19:55:23 -0500

    some change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,35 @@
-import { FC, useEffect, useMemo, useState } from 'react';
+import { FC, useEffect, useMemo, useRef, useState } from 'react';
 import './Header.sass';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header: FC = () => {
     const navigate = useNavigate();
     const [activeTabName, setActiveTabName] = useState<any>({ innerText:'Inicio' });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
     const homeClickEventHandler = ()=>{
         navigate('/');
+        setIsMenuOpen(false);
+    };
+    const toggleMenu = () => {
+        console.log('click');
+        setIsMenuOpen(!isMenuOpen);
     };
     const aboutUsEventHandler = ()=>{
         navigate('/about-us');
+        setIsMenuOpen(false);
     };
     const cvEventHandler = ()=>{
         window.open('/BROCHURE.pdf', '_blank');
+        setIsMenuOpen(false);
     };
     const ourServicesEventHandler = ()=>{
         navigate('/services');
+        setIsMenuOpen(false);
     };
     const ourPlansEventHandler = ()=>{
         navigate('/plans');
+        setIsMenuOpen(false);
     };
     const routes = useMemo(()=> [
         {
@@ -47,6 +58,19 @@ const Header: FC = () => {
         console.log(activeTabName);
         setActiveTabName(activeTabName);
     }, [location.pathname, routes]);
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuRef]);
     return (
         <div className="header__container--light">
             <div className="header__logotype">
@@ -61,12 +85,25 @@ const Header: FC = () => {
                     />
                 </picture>
             </div>
-            <div className="menu__container">
-                <div className={`menu__item ${activeTabName.innerText === 'Inicio' ? 'active' : ''}`} onClick={homeClickEventHandler}>Inicio</div>
-                <div className={`menu__item ${activeTabName.innerText === 'Nuestros servicios' ? 'active' : ''}`} onClick={ourServicesEventHandler}>Nuestros servicios</div>
-                <div className={`menu__item ${activeTabName.innerText === 'Planes' ? 'active' : ''}`} onClick={ourPlansEventHandler}>Planes</div>
-                <div className={`menu__item ${activeTabName.innerText === 'Acerca de nosotros' ? 'active' : ''}`} onClick={aboutUsEventHandler}>Acerca de nosotros</div>
-                <div className={`menu__item ${activeTabName.innerText === 'Grupos de usuarios' ? 'active' : ''}`} onClick={cvEventHandler}>Brochure</div>
+            <div className="hamburger" onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <div  ref={menuRef} className={`menu__container ${isMenuOpen ? 'menu__opened' : ''}`}>
+                <div className={`menu__item first ${activeTabName.innerText === 'Inicio' ? 'active' : ''}`}
+                     onClick={homeClickEventHandler}>Inicio
+                </div>
+                <div className={`menu__item ${activeTabName.innerText === 'Nuestros servicios' ? 'active' : ''}`}
+                     onClick={ourServicesEventHandler}>Nuestros servicios
+                </div>
+                <div className={`menu__item ${activeTabName.innerText === 'Planes' ? 'active' : ''}`}
+                     onClick={ourPlansEventHandler}>Planes
+                </div>
+                <div className={`menu__item ${activeTabName.innerText === 'Acerca de nosotros' ? 'active' : ''}`}
+                     onClick={aboutUsEventHandler}>Acerca de nosotros
+                </div>
+                <div className={`menu__item last ${activeTabName.innerText === 'Grupos de usuarios' ? 'active' : ''}`}
+                     onClick={cvEventHandler}>Brochure
+                </div>
             </div>
         </div>
     );
